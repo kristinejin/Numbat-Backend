2a98cc2d6d0ee61449e9da5804a92d1c94f6fa1a
Surya Gunasekaran <z5248110@ad.unsw.edu.au>
2022-03-27 11:22:51 +1100

    changed indents to spaces

--- src/FlaskApp.py
+++ src/FlaskApp.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return ("Hello World")
+    return ("Hello World")
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run()
+    app.debug = True
+    app.run()
