624a3916279e1ac81e26a3ce2867c3cd75bf17ea
Surya Gunasekaran <z5248110@ad.unsw.edu.au>
2022-03-31 14:17:24 +1100

    request routes storage API

--- src/FlaskApp.py
+++ src/FlaskApp.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from src.auth import Login, CreateAccount
+import requests
 
 
 app = Flask(__name__)
@@ -41,13 +42,65 @@ def Register():
     else:
         return render_template("RegisterHome.html")
 
-@app.route("/Home", methods=["POST,GET"])
+@app.route("/Home", methods=["POST", "GET"])
 @login_required
 def Home():
     if request.method == 'POST':
         pass
     else:
         return render_template("HomePage.html")
+
+@app.route("/Extract", methods=["GET","POST"])
+def Extract():
+    if request.method == 'POST':
+        FileName = request.form["FileName"]
+        Password = request.form["Password"]
+        url = "https://teamfudgeh17a.herokuapp.com/extract"
+        data = {"FileName":FileName, "Password": Password}
+        try:
+            r = requests.post(url,data)
+            return render_template("ExtractOutput.html", XML = r.text)
+        except Exception as e:
+            return render_template("Error.html", Error = e)         
+    else:
+        return render_template("extractMain.html")
+
+@app.route("/Store", methods=["GET","POST"])
+def store():
+    if request.method == 'POST':
+        FileName = request.form["FileName"]
+        Password = request.form["Password"]
+        Xml = request.form["XML"]
+        url = "https://teamfudgeh17a.herokuapp.com/store"
+        data = {"FileName":FileName, "XML":Xml, "Password": Password}
+        try:
+            r = requests.post(url,data)
+            if r.status_code == 200:
+                return render_template("StoreOutput.html")
+            else:
+                return render_template("Error.html", Error = 'Not able to save File')        
+        except Exception as e:
+            return render_template("Error.html", Error = e)         
+    else:
+        return render_template("storeMain.html")
+
+# @app.route("/Delete", methods=["GET","POST"])
+# def delete():
+#     if request.method == 'POST':
+#         FileName = request.form["FileName"]
+#         Password = request.form["Password"]
+#         url = "https://teamfudgeh17a.herokuapp.com/store"
+#         data = {"FileName":FileName, "Password": Password}
+#         try:
+#             r = requests.post(url,data)
+#             if r.status_code == 200:
+#                 return render_template("StoreOutput.html")
+#             else:
+#                 return render_template("Error.html", Error = 'Not able to save File')        
+#         except Exception as e:
+#             return render_template("Error.html", Error = e)         
+#     else:
+#         return render_template("storeMain.html")   
 
 if __name__ == '__main__':
     app.debug = True
