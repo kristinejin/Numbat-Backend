0b0f8bde692cc9166e3316986ebca5d16a0f094d
Matthew Druckman <matthew.druckman@gmail.com>
2022-04-02 09:24:29 +1100

    fixed missing auth in testing file

--- test/num_rendered_or_stored_test.py
+++ test/num_rendered_or_stored_test.py
@@ -16,27 +16,29 @@ def test_store_zero(Account):
 
 def test_store_one(Account, client):
     Filename = (''.join(random.choice(string.ascii_lowercase) for i in range(9)))
-    resp = client.post('/Store', data={'FileName': Filename, 'Password': Account[2], 'XML': xml_as_string})
+    resp = client.post('/Register', data={'UserName': Account[0], 'Password': Account[1], 'CompanyCode': Account[2]})
     assert resp.status_code == 302
+    resp = client.post('/Store', data={'FileName': Filename, 'Password': Account[2], 'XML': xml_as_string})
+    assert resp.status_code == 200
     assert check_okay(Account[0], Account[2], 'store') == 'Success'
 
 def test_store_too_may(Account, client):
     Filename = (''.join(random.choice(string.ascii_lowercase) for i in range(9)))
+    resp = client.post('/Register', data={'UserName': Account[0], 'Password': Account[1], 'CompanyCode': Account[2]})
+    assert resp.status_code == 302
     resp = client.post('/Store', data={'FileName': Filename, 'Password': Account[2], 'XML': xml_as_string})
-    assert resp.status_code == 302
+    assert resp.status_code == 200
     resp = client.post('/Store', data={'FileName': Filename + 'a', 'Password': Account[2], 'XML': xml_as_string})
-    assert resp.status_code == 302
+    assert resp.status_code == 200
     resp = client.post('/Store', data={'FileName': Filename + 'b', 'Password': Account[2], 'XML': xml_as_string})
-    assert resp.status_code == 302
+    assert resp.status_code == 200
     resp = client.post('/Store', data={'FileName': Filename + 'c', 'Password': Account[2], 'XML': xml_as_string})
-    assert resp.status_code == 302
+    assert resp.status_code == 200
     resp = client.post('/Store', data={'FileName': Filename + 'd', 'Password': Account[2], 'XML': xml_as_string})
-    assert resp.status_code == 302
-    resp = client.post('/Store', data={'FileName': Filename + 'e', 'Password': Account[2], 'XML': xml_as_string})
-    assert resp.status_code == 302
-    print(Account[2])
+    assert resp.status_code == 200
     assert check_okay(Account[0], Account[2], 'store') == 'Fail'
     
+    # THE FILES BEING STORED ARE NOT SHOWING UP IN THE DATABASE THAT I HAVE
 
 
 
