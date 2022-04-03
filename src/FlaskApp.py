fafa0f12eb341a86ce7dbfdcfd1b0bf2fd0ae54a
Kristine Jin <z5362038@ad.unsw.edu.au>
2022-04-03 04:21:53 +0000

    Resolve SE2Y22G7-71 "Add rendering api routes to server"

--- src/FlaskApp.py
+++ src/FlaskApp.py
@@ -5,6 +5,7 @@ from src.receive import receiveAndStore
 from src.check_num_render_or_store import check_okay
 import requests
 import functools
+import json
 
 
 app = Flask(__name__)
@@ -166,22 +167,71 @@ def logout():
     else:
         return render_template("Logout.html")
 
+
 @app.route("/receive", methods=["POST"])
 def receive_data():
-   
-    xml = request.form['xml_attachments']
-    email = request.form['sender_address']
-    
+
+    xml = request.json['xml_attachments']
+    email = request.json['sender_address']
+
     companyCode = receiveAndStore(email)
 
     url = "https://teamfudgeh17a.herokuapp.com/store"
-    data = {"FileName":request.form['id'], "XML":xml, "Password": companyCode}
+    data = {"FileName": request.json['processed_at_timestamp'],
+            "XML": xml, "Password": companyCode}
     try:
-        r = requests.post(url,data)
-        json.dumps(r)       
+        r = requests.post(url, data)
+        if r.status_code == 200:
+            return 200
     except Exception as e:
-        return render_template("Error.html", Error = e)         
+        return render_template("Error.html", Error=e)
 
-if __name__ == "__main__":
+
+@app.route("/Render", methods=["GET", "POST"])
+@login_required
+def rendering():
+    if request.method == 'POST':
+        FileName = request.form["FileName"]
+        Password = request.form["Password"]
+
+        # File type can only be pdf/html/json
+        FileType = request.form["FileType"]
+
+        extract_url = "https://teamfudgeh17a.herokuapp.com/extract"
+        extract_data = {"FileName": FileName, "Password": Password}
+
+        # API: https://app.swaggerhub.com/apis/r-kaisar/e-invoice-rendering/1.0.0#/rendering
+        rendering_upload_url = "https://e-invoice-rendering-brownie.herokuapp.com/invoice/rendering/upload"
+        rendering_download_url = "https://e-invoice-rendering-brownie.herokuapp.com/invoice/rendering/download"
+
+        try:
+            # get the invoice as a string in the storage db
+            file_str = requests.post(extract_url, extract_data)
+            # print(file_str.text)
+            # convert string to a binary xml file
+            with open("str_to_xml.xml", "w") as f:
+                f.write(str(file_str.text))
+            # payload for rendering upload request
+            # rendering upload ret type: {"file_ids": [int_file_id]}
+            pload = {'file': open('str_to_xml.xml', 'r')}
+            rendering_upload_request = requests.post(
+                rendering_upload_url, files=pload)
+            rendering_upload = json.loads(rendering_upload_request.text)
+            # call rendering download endpoint
+            # rendered file in rendering_download
+            rendering_download = requests.get(
+                rendering_download_url, params={"file_id": rendering_upload["file_ids"][0], "file_type": FileType})
+            assert rendering_download.status_code == 200
+            if check_okay("None", Password, "render") == "Fail":
+                return render_template("Error.html", Error="Rendered Invoice Quota is FULL")
+
+            return render_template("renderOutput.html", file_path=rendering_download.url)
+        except Exception as e:
+            return render_template("Error.html", Error=e)
+    else:
+        return render_template("renderMain.html")
+
+
+if __name__ == '__main__':
     app.debug = True
     app.run()
